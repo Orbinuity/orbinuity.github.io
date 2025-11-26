c31e8671190b68a84a9ca449bd0bd479e3061193
Flurop <flora@ratgers.nl>
2025-11-26 21:01:09 +0100

    Script for page

---
+++ projects.js
@@ -0,0 +1,19 @@
+fetch('/htmls/')
+    .then(res => res.text())
+    .then(html => {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+        const links = doc.querySelectorAll('a');
+        const ul = document.getElementById('project-list');
+        links.forEach(link => {
+            const href = link.getAttribute('href');
+            if(href.endsWith('.html')) {
+                const li = document.createElement('li');
+                const a = document.createElement('a');
+                a.href = '/htmls/' + href;
+                a.textContent = href;
+                li.appendChild(a);
+                ul.appendChild(li);
+            }
+        });
+    });
