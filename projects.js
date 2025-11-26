c11987b215ee2363378ef9f2052be630a3b06479
Flurop <flora@ratgers.nl>
2025-11-26 21:16:27 +0100

    Code to automaricly load prjects

--- projects.js
+++ projects.js
@@ -1,19 +1,13 @@
-fetch('/project/')
-    .then(res => res.text())
-    .then(html => {
-        const parser = new DOMParser();
-        const doc = parser.parseFromString(html, 'text/html');
-        const links = doc.querySelectorAll('a');
-        const ul = document.getElementById('project-list');
-        links.forEach(link => {
-            const href = link.getAttribute('href');
-            if(href.endsWith('.html')) {
-                const li = document.createElement('li');
-                const a = document.createElement('a');
-                a.href = '/htmls/' + href;
-                a.textContent = href;
-                li.appendChild(a);
-                ul.appendChild(li);
-            }
+fetch('/project/files.json')
+    .then(res => res.json())
+    .then(files => {
+        const ul = document.getElementById('file-list');
+        files.forEach(file => {
+            const li = document.createElement('li');
+            const a = document.createElement('a');
+            a.href = '/project/' + file;
+            a.textContent = file;
+            li.appendChild(a);
+            ul.appendChild(li);
         });
     });
