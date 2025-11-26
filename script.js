0d1f388341528606195eb2436f0303479d653ff0
Flurop <flora@ratgers.nl>
2025-11-26 21:06:01 +0100

    Fixed path

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const header = document.getElementById('header');
 
 const userName = header.dataset.id;
 
-fetch('nogo/header.html')
+fetch('/nogo/header.html')
     .then(response => response.text())
     .then(html => {
         document.getElementById('header').innerHTML = html;
