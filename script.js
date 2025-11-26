32031e9c373bbf8a7c094a93da125d61b1fe6e43
Flurop <flora@ratgers.nl>
2025-11-26 17:15:42 +0100

    FIx underline

--- script.js
+++ script.js
@@ -1,13 +1,15 @@
+const scriptTag = document.currentScript;
+
+const userName = scriptTag.dataset.id;
+
 fetch('nogo/header.html')
     .then(response => response.text())
     .then(html => {
         document.getElementById('header').innerHTML = html;
 
         const links = document.querySelectorAll('nav a');
-        const currentPath = window.location.pathname;
-        print(currentPath)
         links.forEach(link => {
-            if(link.getAttribute('href') === currentPath) {
+            if(link.getAttribute('href') === userName) {
                 link.style.textDecoration = 'underline';
             }
         });
