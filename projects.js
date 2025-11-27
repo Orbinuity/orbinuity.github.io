e6751ec9cb934229ce9e1f64f8cc11f01d4d30c5
Flurop <flora@ratgers.nl>
2025-11-27 11:37:16 +0100

    Ok, i think it works now!

--- projects.js
+++ projects.js
@@ -3,13 +3,11 @@ fetch('/nogo/projects.json')
     .then(projects => {
         const cardList = document.getElementById('project-list');
         projects.forEach(project => {
-            const a = document.createElement('a');
             const button = document.createElement('button');
-            
-            a.href = `/project/${project[0]}`
+            button.addEventListener("onclick", () => {
+                window.location.href = `/project/${project[0]}`;
+            });
             button.textContent = project[1];
-
-            a.appendChild(button)
-            cardList.appendChild(a);
+            cardList.appendChild(button);
         });
     });
