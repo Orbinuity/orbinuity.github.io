c4c2b5dee91907bd9bfd317bdbfe360681f6e9af
Flurop <flora@ratgers.nl>
2025-11-27 09:16:01 +0100

    Fixed ul id

--- projects.js
+++ projects.js
@@ -1,7 +1,7 @@
 fetch('/project/files.json')
     .then(res => res.json())
     .then(files => {
-        const ul = document.getElementById('file-list');
+        const ul = document.getElementById('project-list');
         files.forEach(file => {
             const li = document.createElement('li');
             const a = document.createElement('a');
