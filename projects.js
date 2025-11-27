5811ebae68a391982aeb1b292ceca8a53c28943e
Flurop <flora@ratgers.nl>
2025-11-27 09:51:40 +0100

    Fixed button

--- projects.js
+++ projects.js
@@ -4,7 +4,7 @@ fetch('/project/files.json')
         const cardList = document.getElementById('project-list');
         files.forEach(file => {
             const button = document.createElement('button');
-            button.onclick = `window.location.href = '/project/${file[0]}';`;
+            button.click = `window.location.href = '/project/${file[0]}';`;
             button.textContent = file[1];
             cardList.appendChild(button);
         });
