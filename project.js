9bdc0212ab3bcb8373b8a854bc679eb66062a3ef
Flurop <flora@ratgers.nl>
2026-04-08 17:42:08 +0200

    Now using a full dynamic project system!

---
+++ project.js
@@ -0,0 +1,44 @@
+function renderContent() {
+    const route = window.location.hash.substring(1); 
+    const title = document.getElementById('project-title');
+    const name = document.getElementById('project-name')
+    const description = document.getElementById('project-description');
+    const buttons = document.getElementById('project-buttons');
+
+    if (!route || route === '/') {
+        title.textContent = "Orbinuity - Project";
+        name.textContent = "Project"
+        description.textContent = ""
+        return;
+    } else {
+        fetch('/nogo/projects.json')
+        .then(res => res.json())
+        .then(projects => {
+            const project = projects[route]
+
+            title.textContent = `Orbinuity - ${project[0]}`
+            name.textContent = project[0]
+            description.textContent = project[1]
+
+            let pageButtons = [[`https://github.com/Orbinuity/${route}`, "Repo"], [`https://github.com/Orbinuity/${route}/releases`, "Download"]]
+            
+            pageButtons = pageButtons.concat(project[2])
+            
+            buttons.innerHTML = '';
+            pageButtons.forEach(button => {
+                const a = document.createElement('a');
+                a.href = button[0]
+
+                const pageButton = document.createElement('button');
+                pageButton.textContent = button[1];
+
+                a.appendChild(pageButton)
+                buttons.appendChild(a);
+            });
+        });
+    }
+}
+
+renderContent();
+
+window.addEventListener('hashchange', renderContent);
