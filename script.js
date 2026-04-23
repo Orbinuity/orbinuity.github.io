e119dbfa95d8de628d374d604c882a79daa035bd
Flurop <flora@ratgers.nl>
2026-04-23 21:29:59 +0200

    Removed project code

--- script.js
+++ script.js
@@ -1,35 +1,15 @@
-const header = document.getElementById('header');
-
-const userName = header.dataset.id;
-
 fetch('/nogo/header.html')
     .then(response => response.text())
     .then(html => {
-        document.getElementById('header').innerHTML = html;
-    });
-
-fetch('/nogo/projects.json')
-    .then(res => res.json())
-    .then(projects => {
-        const dropdown = document.getElementById('dropdown-content');
-        projects['all'].forEach(project => {
-            const a = document.createElement('a');
-            a.href = `/project#${project}`
-            a.textContent = projects[project][0];
-
-            dropdown.appendChild(a);
-        });
-
-        const links = document.querySelectorAll('nav a, #dropdown-content a');
-        links.forEach(link => {
-            if(link.getAttribute('href') === userName) {
-                link.style.textDecoration = 'underline';
-            }
-        });
+        const range = document.createRange();
+        const fragment = range.createContextualFragment(html);
+        document.getElementById('header').appendChild(fragment);
     });
 
 fetch('/nogo/footer.html')
     .then(response => response.text())
     .then(html => {
-        document.getElementById('footer').innerHTML = html;
+        const range = document.createRange();
+        const fragment = range.createContextualFragment(html);
+        document.getElementById('footer').appendChild(fragment);
     });
