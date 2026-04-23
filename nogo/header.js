f12efa6972e027dd5568ebef27eb2727b5a59d0c
Flurop <flora@ratgers.nl>
2026-04-23 21:29:29 +0200

    Using header.js now

---
+++ nogo/header.js
@@ -0,0 +1,12 @@
+fetch('/nogo/projects.json')
+    .then(res => res.json())
+    .then(projects => {
+        const dropdown = document.getElementById('dropdown-content');
+        projects['all'].forEach(project => {
+            const a = document.createElement('a');
+            a.href = `/project#${project}`
+            a.textContent = projects[project][0];
+
+            dropdown.appendChild(a);
+        });
+    });
