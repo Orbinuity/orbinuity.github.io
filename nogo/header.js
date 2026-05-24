4222adbd736b9ae8f01893cb4b16173b3f365b02
Flurop <flora@ratgers.nl>
2026-05-24 21:01:04 +0200

    Fixed some small issues

--- nogo/header.js
+++ nogo/header.js
@@ -2,7 +2,7 @@ fetch('/nogo/projects.json')
     .then(res => res.json())
     .then(projects => {
         const dropdown = document.getElementById('dropdown-content');
-        projects.keys().forEach(project => {
+        Object.keys(projects).forEach(project => {
             const a = document.createElement('a');
             a.href = `/project#${project}`
             a.textContent = projects[project][0];
