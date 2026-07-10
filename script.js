d8756c5dbd71e39df85dc47a191e7c3004636d9a
Flurop <flora@ratgers.nl>
2026-07-10 22:42:14 +0200

    Added code for news

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
+fetch('http://127.0.0.1:3000/nogo/news.html')
+    .then(response => response.text())
+    .then(html => {
+        if (document.currentScript?.dataset?.nonews !== "true") {
+            const range = document.createRange();
+            const fragment = range.createContextualFragment(html);
+            document.getElementById('header').appendChild(fragment);
+        }
+    });
+
 fetch('/nogo/header.html')
     .then(response => response.text())
     .then(html => {
