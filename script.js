9d6ed3c3d3adee077f3636c65a51ea7b4a31987d
Flurop <flora@ratgers.nl>
2026-07-19 23:44:50 +0200

    Removed news

--- script.js
+++ script.js
@@ -1,13 +1,3 @@
-fetch('https://orbinuity.nl/nogo/news.html')
-    .then(response => response.text())
-    .then(html => {
-        if (document.currentScript?.dataset?.nonews !== "true") {
-            const range = document.createRange();
-            const fragment = range.createContextualFragment(html);
-            document.getElementById('header').appendChild(fragment);
-        }
-    });
-
 fetch('/nogo/header.html')
     .then(response => response.text())
     .then(html => {
