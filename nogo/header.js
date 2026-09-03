0094a9912083b135204e065f5d63e5fcac011566
Flurop <flora@ratgers.nl>
2026-09-03 22:41:54 +0200

    Added accounts and sdk (BETA)

--- nogo/header.js
+++ nogo/header.js
@@ -1,8 +1,17 @@
+const signout = document.getElementById("signout");
+const account = document.getElementById("account");
+const navBar = document.getElementById("nav-bar");
+
+const token = document.cookie
+    .split("; ")
+    .find(row => row.startsWith('token='))
+    ?.split("=")[1];
+
 fetch('/nogo/projects.json')
     .then(res => res.json())
     .then(projects => {
-        const dropdown = document.getElementById('dropdown-content');
         Object.keys(projects).forEach(project => {
+            const dropdown = document.getElementById("dropdown-projects");
             const a = document.createElement('a');
             a.href = `/project?id=${project}`
             a.textContent = projects[project][0];
@@ -10,3 +19,24 @@ fetch('/nogo/projects.json')
             dropdown.appendChild(a);
         });
     });
+
+signout.addEventListener("click", (e) => {
+    e.preventDefault();
+    document.cookie = "token=; max-age=0; path=/";
+    location.reload();
+});
+
+if (!token) {
+    account.remove()
+
+    const login = document.createElement("a");
+    login.href = "/account/login";
+    login.innerText = "Login";
+
+    const signout = document.createElement("a");
+    signout.href = "/account/signup";
+    signout.innerText = "Signup";
+
+    navBar.innerHTML += login.outerHTML;
+    navBar.innerHTML += signout.outerHTML;
+}
