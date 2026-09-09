397ea6bfbe531270c9a3c58c5309dc9549e270f3
Flurop <flora@ratgers.nl>
2026-09-09 21:29:52 +0200

    Fixed

--- account/index.js
+++ account/index.js
@@ -11,7 +11,7 @@ document.addEventListener('DOMContentLoaded', async () => {
     const token = getTokenCookie();
 
     if (!token) {
-        window.location.href = '/account/login.html';
+        window.location.href = '/account/login';
         return;
     }
 
